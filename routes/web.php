b0bfc73f4b1ff6abc6e21c1f0a0b8e45341dc7cd
gilbertobreyerjunior <gilbertobreyer.jr@gmail.com>
2022-05-23 20:00:31 -0300

    Removido comentario

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,7 @@ Route::prefix('/pessoa')->group(function(){
     Route::get('/editar/{id}', 'CadastroPessoa@edit')->name('edit.pessoa');
     Route::post('/editado/{id}', 'CadastroPessoa@update')->name('editado.pessoa');
     Route::delete('/excluir/{id}', 'CadastroPessoa@destroy');
-    // estou usando um name, só que nessa forma usando name, não é dinamico, teria que usar no excluir não uso name?
-// pde usar url
+
 
 
 });
