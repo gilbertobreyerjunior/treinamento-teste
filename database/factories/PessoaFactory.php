df3ebf9e691715b1b9a0f47f2b4e03c9cedd08c9
gilbertobreyerjunior <gilbertobreyer.jr@gmail.com>
2022-06-05 12:39:18 -0300

    Criada a factory Pessoa

---
+++ database/factories/PessoaFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Pessoa;
+use Faker\Generator as Faker;
+
+$factory->define(Pessoa::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
