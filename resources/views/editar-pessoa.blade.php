39d481b560535f84461c6d354e4b17e9b22e643f
gilbertobreyerjunior <gilbertobreyer.jr@gmail.com>
2022-06-07 20:53:17 -0300

    Adicionada mascara para cpf em editar pessoas

--- resources/views/editar-pessoa.blade.php
+++ resources/views/editar-pessoa.blade.php
@@ -1,6 +1,5 @@
 @extends('layout.app')
 
-
 @section('title', ' Editar Pessoa')
 
 @section('content')
@@ -14,31 +13,24 @@
                 <input type="text" class="form-control {{ $errors->has('nomePessoa') ? 'is-invalid' :''  }} "
                     name="nomePessoa" value="{{$pe->nome}}" id="nomePessoa">
 
-
                 @if($errors->has('nomePessoa'))
 
-
                 <div class="invalid-feedback">
-
 
                     {{ $errors ->first('nomePessoa') }}
 
                 </div>
                 @endif
 
-
             </div>
 
             <div class="form-group">
                 <label for="cpfPessoa">CPF</label>
-                <input type="text" class="form-control {{ $errors->has('cpfPessoa') ? 'is-invalid' :''  }} "
-                    name="cpfPessoa" value="{{$pe->cpf}}" id="cpfPessoa">
+                <input type="text" class="form-control {{ $errors->has('cpfPessoa') ? 'is-invalid' :''  }}" name="cpfPessoa" value="{{$pe->cpf}}" id="cpf" onkeyup="cpfCheck(this)" onkeydown="javascript: fMasc( this, mCPF );" placeholder="Digite o CPF"> <span id="cpfResponse"></span>
 
                 @if($errors->has('cpfPessoa'))
 
-
                 <div class="invalid-feedback">
-
 
                     {{ $errors ->first('cpfPessoa') }}
 
@@ -52,13 +44,9 @@
                 <input type="text" class="form-control {{ $errors->has('telefonePessoa') ? 'is-invalid' :''  }} "
                     name="telefonePessoa" value="{{$pe->telefone}}" id="telefonePessoa">
 
-
-
                 @if($errors->has('telefonePessoa'))
 
-
                 <div class="invalid-feedback">
-
 
                     {{ $errors ->first('telefonePessoa') }}
 
@@ -67,19 +55,14 @@
 
             </div>
 
-
             <div class="form-group">
                 <label for="cepPessoa">CEP</label>
                 <input type="text" class="form-control {{ $errors->has('cepPessoa') ? 'is-invalid' :''  }} "
                     name="cepPessoa" value="{{$pe->cep}}" id="cep">
 
-
-
                 @if($errors->has('cepPessoa'))
 
-
                 <div class="invalid-feedback">
-
 
                     {{ $errors ->first('cepPessoa') }}
 
@@ -88,19 +71,14 @@
 
             </div>
 
-
             <div class="form-group">
                 <label for="uf">Estado</label>
                 <input type="text" class="form-control {{ $errors->has('uf') ? 'is-invalid' :''  }} " name="uf"
                     value="{{$pe->uf}}" id="uf">
 
-
-
                 @if($errors->has('uf'))
 
-
                 <div class="invalid-feedback">
-
 
                     {{ $errors ->first('uf') }}
 
@@ -114,13 +92,9 @@
                 <input type="text" class="form-control {{ $errors->has('cidade') ? 'is-invalid' :''  }} " name="cidade"
                     value="{{$pe->cidade}}" id="cidade">
 
-
-
                 @if($errors->has('cidade'))
 
-
                 <div class="invalid-feedback">
-
 
                     {{ $errors ->first('cidade') }}
 
@@ -134,13 +108,9 @@
                 <input type="text" class="form-control {{ $errors->has('bairro') ? 'is-invalid' :''  }} " name="bairro"
                     value="{{$pe->bairro}}" id="bairro">
 
-
-
                 @if($errors->has('bairro'))
 
-
                 <div class="invalid-feedback">
-
 
                     {{ $errors ->first('bairro') }}
 
@@ -156,9 +126,7 @@
 
                 @if($errors->has('logradouro'))
 
-
                 <div class="invalid-feedback">
-
 
                     {{ $errors ->first('logradouro') }}
 
@@ -175,6 +143,9 @@
         {{--  <a href="" class="btn btn-default">Voltar</a>  --}}
     </div>
 
+    <script src="/jquery.min.js"></script>
+    <script src="/js/viacep/viacep.js"></script>
+    <script src="/js/validacpf/validacpf.js"></script>
 
     @endsection
     <!-- Encerra a seção -->
